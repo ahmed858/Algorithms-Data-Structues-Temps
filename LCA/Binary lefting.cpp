4199692157a36aa797d9805dbdcb83cc1bdaef81
Ahmed Abd Elazez Hassan <59138218+ahmed858@users.noreply.github.com>
2021-09-27 20:34:28 +0200

    Update Binary lefting.cpp

--- LCA/Binary lefting.cpp
+++ LCA/Binary lefting.cpp
@@ -1,8 +1,14 @@
+/*
+Spoj: Lowest Common Ancestor
+*/
+
+
 #include<bits/stdc++.h>
 #define ll				    long long
 #define endl			    '\n'
 #define sz(x)			    x.size()
 using namespace std;
+
 const ll  N=2e5+5,M=1e5+5,LG=17;
 vector<int> adj [N];
 int par[N][LG],depth[N],n;
@@ -18,7 +24,6 @@ void dfs(int u) {
         dfs(v);
     }
 }
-
 int getKthParent(int u,int k) {
     while (k) {
         int bit = __lg(k);
@@ -27,35 +32,45 @@ int getKthParent(int u,int k) {
     }
     return u;
 }
-
-int main() {
-    scanf("%d", &n);
-    int u, v;
-    for (int i = 1; i < n; i++) {
-        scanf("%d%d", &u, &v);
-        adj[u].emplace_back(v);
-        adj[v].emplace_back(u);
+int LCA(int u,int v) {
+    if (depth[v] > depth[u])swap(u, v);
+    int d = depth[u] - depth[v];
+    if (d) u = getKthParent(u, d);// now its equal
+    int l = 0, r = depth[u], md, k = depth[u];
+    while (l <= r) {
+        md = (l + r) / 2;
+        if (getKthParent(u, md) == getKthParent(v, md)) {
+            k = md;
+            r =md - 1;
+        } else l = md + 1;
     }
-  
-    dfs(1);
-  
-  for (int i = 1; i <= n; i++) {
-        printf("%d:", i);
-        for (int j = 1; j < 6; j++) {
-            printf("%d ", getKthParent(i,j));
-        }
-        puts("");
-    }
-    return 0;
+    return getKthParent(u, k);
 }
-/*
-9
-1 2
-1 3
-1 4
-3 7
-3 6
-4 5
-7 8
-8 9
-*/
+int main() {
+
+    int t, c = 1;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d",  &n);
+        for (int i = 1; i <= n; i++)
+            adj[i].clear();
+        int u, v;
+        for (int i = 1; i <= n; i++) {
+            adj[i].clear();
+            scanf("%d", &u);
+            while (u--) {
+                scanf("%d", &v);
+                adj[i].push_back(v);
+                adj[v].push_back(i);
+            }
+        }
+        dfs(1);
+        int q;
+        scanf("%d", &q);
+        printf("Case %d:\n", c++);
+        while (q--) {
+            scanf("%d%d", &u, &v);
+            printf("%d\n", LCA(u, v));
+        }
+    }
+}
